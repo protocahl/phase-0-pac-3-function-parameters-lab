dfaba2e73bbcdce75a75a32cc2fe17615f25be55
Armani Simmons <fibonaccy@protonmail.com>
2023-10-02 12:25:00 -0400

    complete

---
+++ index.js
@@ -0,0 +1,23 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+  // Define a function that uses a parameter
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  // Define a function that uses two parameters
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Define a function with a parameter that has a default value
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+ 
+  
